cb64678f2f37bef99c80c711c7d47429f6220775
amanqs <116822442+amanqs@users.noreply.github.com>
2024-09-25 11:22:01 +0700

    Update message_reactions.py

--- pyrogram/types/messages_and_media/message_reactions.py
+++ pyrogram/types/messages_and_media/message_reactions.py
@@ -1,22 +1,23 @@
-#  Pyrogram - Telegram MTProto API Client Library for Python
+#  Pyrofork - Telegram MTProto API Client Library for Python
 #  Copyright (C) 2017-present Dan <https://github.com/delivrance>
+#  Copyright (C) 2022-present Mayuri-Chan <https://github.com/Mayuri-Chan>
 #
-#  This file is part of Pyrogram.
+#  This file is part of Pyrofork.
 #
-#  Pyrogram is free software: you can redistribute it and/or modify
+#  Pyrofork is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU Lesser General Public License as published
 #  by the Free Software Foundation, either version 3 of the License, or
 #  (at your option) any later version.
 #
-#  Pyrogram is distributed in the hope that it will be useful,
+#  Pyrofork is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU Lesser General Public License for more details.
 #
 #  You should have received a copy of the GNU Lesser General Public License
-#  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
+#  along with Pyrofork.  If not, see <http://www.gnu.org/licenses/>.
 
-from typing import Optional, List
+from typing import Optional, List, Dict
 
 import pyrogram
 from pyrogram import raw, types
@@ -29,6 +30,9 @@ class MessageReactions(Object):
     Parameters:
         reactions (List of :obj:`~pyrogram.types.Reaction`):
             Reactions list.
+
+        top_reactors (List of :obj:`~pyrogram.types.MessageReactor`):
+            Top reactors.
     """
 
     def __init__(
@@ -36,21 +40,30 @@ class MessageReactions(Object):
         *,
         client: "pyrogram.Client" = None,
         reactions: Optional[List["types.Reaction"]] = None,
+        top_reactors: Optional[List["types.MessageReactor"]] = None
     ):
         super().__init__(client)
 
         self.reactions = reactions
+        self.top_reactors = top_reactors
 
     @staticmethod
     def _parse(
         client: "pyrogram.Client",
-        message_reactions: Optional["raw.base.MessageReactions"] = None
+        message_reactions: Optional["raw.base.MessageReactions"] = None,
+        users: Optional[Dict[int, "raw.types.User"]] = None
     ) -> Optional["MessageReactions"]:
         if not message_reactions:
             return None
 
         return MessageReactions(
             client=client,
-            reactions=[types.Reaction._parse_count(client, reaction)
-                       for reaction in message_reactions.results]
+            reactions=[
+                types.Reaction._parse_count(client, reaction)
+                for reaction in message_reactions.results
+            ],
+            top_reactors=[
+                types.MessageReactor._parse(client, reactor, users)
+                for reactor in message_reactions.top_reactors
+            ]
         )
